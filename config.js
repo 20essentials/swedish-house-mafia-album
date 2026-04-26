e1138408f510f866b18d0417acf50c111ff7ced8
DominicDev <s0l0estudi4r@gmail.com>
2026-04-26 23:09:32 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'SWEDISH HOUSE MAFIA';
+export let nameSongs = [
+	"DON'T YOU WORRY CHILD", /*1*/
+	"SAVE THE WORLD", /*2*/
+	"GREYHOUND", /*3*/
+	"ONE", /*4*/
+	"ANTIDOTE", /*5*/
+	"MOTH TO A FLAME", /*6*/
+	"HEAVEN TAKES YOU HOME", /*7*/
+	"REDLIGHT", /*8*/
+	"FOR YOU", /*9*/
+	"CALLING ON", /*10*/
+	"DON'T GO MAD", /*11*/
+	"MAFIA", /*12*/
+	"HOME", /*13*/
+	"PARADISE AGAIN", /*14*/
+	"CAN U FEEL IT", /*15*/
+	"TIME", /*16*/
+	"ANOTHER MINUTE", /*17*/
+	"LIFETIME", /*18*/
+	"RAY OF SOLAR", /*19*/
+	"LIONESS", /*20*/
+]
